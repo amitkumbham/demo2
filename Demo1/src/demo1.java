f7983e938eb70f0502a6dfcf307236a59c67ba76
amitkumbham <amitkumbham@localhost>
2021-01-17 09:06:44 -0500

    revert back changes

--- Demo1/src/demo1.java
+++ Demo1/src/demo1.java
@@ -4,9 +4,6 @@ public class demo1 {
 	public static void main(String[] args) {
 		//main call with this message
 		System.out.println("my ffirst commit");
-		
-		System.out.println("my second  commit");
-		System.out.println("my thjird commit");
 
 	}
 
