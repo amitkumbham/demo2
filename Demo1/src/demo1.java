d8ec3a37ce726ec717c793d2968a4ded4470cbdb
amitkumbham <amitkumbham@localhost>
2021-01-17 09:04:47 -0500

    second commit for two sysout

--- Demo1/src/demo1.java
+++ Demo1/src/demo1.java
@@ -4,6 +4,9 @@ public class demo1 {
 	public static void main(String[] args) {
 		//main call with this message
 		System.out.println("my ffirst commit");
+		
+		System.out.println("my second  commit");
+		System.out.println("my thjird commit");
 
 	}
 
