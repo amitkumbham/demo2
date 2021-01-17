a8b4ab1bd414fe8629675c3ca73837c0e79b25e7
amitkumbham <amitkumbham@localhost>
2021-01-17 09:08:58 -0500

    revert back to orginal after fouth commit

--- Demo1/src/demo1.java
+++ Demo1/src/demo1.java
@@ -4,11 +4,6 @@ public class demo1 {
 	public static void main(String[] args) {
 		//main call with this message
 		System.out.println("my ffirst commit");
-		
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
-		
 
 	}
 
