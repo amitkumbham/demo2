f35660a156167107f0227073f7b9ac85048adc60
amitkumbham <amitkumbham@localhost>
2021-01-18 23:04:35 -0500

    latest

--- Demo1/src/demo1.java
+++ Demo1/src/demo1.java
@@ -4,6 +4,7 @@ public class demo1 {
 	public static void main(String[] args) {
 		//main call with this message
 		System.out.println("my ffirst commit");
+		System.out.println("my second commit");
 
 	}
 
